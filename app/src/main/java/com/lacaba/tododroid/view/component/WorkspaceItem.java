2342a15fb39cbb4208f85c0fc7cb5127126da322
RedFlameKen <kabalacaba@gmail.com>
2025-07-14 21:59:26 +0800

    feat: add workspace items

---
+++ app/src/main/java/com/lacaba/tododroid/view/component/WorkspaceItem.java
@@ -0,0 +1,55 @@
+package com.lacaba.tododroid.view.component;
+
+import com.lacaba.tododroid.R;
+
+import com.lacaba.tododroid.model.Workspace;
+
+import android.content.Context;
+import android.widget.TextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+public class WorkspaceItem extends ConstraintLayout {
+
+    private Workspace workspace;
+    private TextView nameLabel;
+
+    public WorkspaceItem(Context context, Workspace workspace){
+        super(context);
+        this.workspace = workspace;
+        initComponents();
+    }
+
+    private void initComponents(){
+        LayoutParams ownParams = new LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+                );
+        setLayoutParams(ownParams);
+        setBackgroundColor(getContext().getColor(R.color.gruvbox_black));
+        nameLabel = new TextView(getContext());
+        nameLabel.setId(generateViewId());
+        nameLabel.setText(workspace != null ? workspace.getName() : "Undefined");
+        nameLabel.setTextColor(getContext().getColor(R.color.gruvbox_white_light));
+        LayoutParams params = new LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+                );
+        nameLabel.setLayoutParams(params);
+        ConstraintSet set = new ConstraintSet();
+        addView(nameLabel);
+        set.clone(this);
+        set.connect(nameLabel.getId(), ConstraintSet.TOP, this.getId(), ConstraintSet.TOP);
+        set.connect(nameLabel.getId(), ConstraintSet.START, this.getId(), ConstraintSet.START);
+        set.connect(nameLabel.getId(), ConstraintSet.BOTTOM, this.getId(), ConstraintSet.BOTTOM);
+        set.applyTo(this);
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        setOnClickListener(view -> {
+        });
+    }
+    
+}
--- app/src/main/java/com/lacaba/tododroid/view/fragment/DashboardFragment.java
+++ app/src/main/java/com/lacaba/tododroid/view/fragment/DashboardFragment.java
@@ -1,24 +1,52 @@
 package com.lacaba.tododroid.view.fragment;
 
+import java.util.ArrayList;
+
 import com.lacaba.tododroid.R;
+import com.lacaba.tododroid.model.Workspace;
+import com.lacaba.tododroid.view.component.WorkspaceItem;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import androidx.fragment.app.Fragment;
 
 public class DashboardFragment extends Fragment {
 
+    private ArrayList<WorkspaceItem> workspaceItems;
+    private LinearLayout panel;
+
     public DashboardFragment(){
         super(R.layout.fragment_dashboard);
+        this.workspaceItems = new ArrayList<>();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
+        if(panel != null){
+
+            if(panel.getChildCount() > 0){
+                while(panel.getChildCount() > 0){
+                    panel.removeView(panel.getChildAt(0));
+                }
+            }
+        }
+        panel = getActivity().findViewById(R.id.dashboard_content_panel);
+
+        for (int i = 0; i < 5; i++) {
+            Workspace ws = new Workspace.Builder()
+                .name(new StringBuilder().append("workspace ").append(i).toString())
+                .build();
+            workspaceItems.add(new WorkspaceItem(requireContext(), ws));
+            panel.addView(workspaceItems.get(i));
+        }
+
     }
-    
+
 }
